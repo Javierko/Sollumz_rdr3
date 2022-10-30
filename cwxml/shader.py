5b09e6aac3d8e2d7f3e7df30daf7cc9c6a270ebe
Ktoś <43894510+Ktos93@users.noreply.github.com>
2022-10-30 20:43:06 +0100

    some shaders

--- cwxml/shader.py
+++ cwxml/shader.py
@@ -88,27 +88,24 @@ class ShaderManager:
                 "terrain_cb_w_4lyr_pxm.sps", "terrain_cb_w_4lyr_cm_pxm.sps", "terrain_cb_w_4lyr_cm_tnt.sps", "terrain_cb_w_4lyr_cm_pxm_tnt.sps", "terrain_cb_w_4lyr_cm.sps",
                 "terrain_cb_w_4lyr_2tex.sps", "terrain_cb_w_4lyr_2tex_blend.sps", "terrain_cb_w_4lyr_2tex_blend_lod.sps", "terrain_cb_w_4lyr_2tex_blend_pxm.sps",
                 "terrain_cb_w_4lyr_2tex_blend_pxm_spm.sps", "terrain_cb_w_4lyr_2tex_pxm.sps", "terrain_cb_4lyr.sps", "terrain_cb_w_4lyr_spec_int_pxm.sps",
-                "terrain_cb_w_4lyr_spec_int.sps", "terrain_cb_4lyr_lod.sps", "terrain_lod_2lyr_grass_wetness", "terrain_lod_2lyr", "terrain_lod_grass_wetness", "terrain_slod", "terrain_uber_2lyr_pxm_hbb", 
-                "terrain_uber_3+1lyr_snow_dm_hbb", "terrain_uber_4lyr_hbb", "terrain_uber_4lyr_mud_dm_hbb", "terrain_uber_4lyr_pxm_hbb", "terrain_uber_4lyr_pxm", "terrain_uber_4lyr_quicksand_dm_hbb", 
-                "terrain_uber_4lyr_snow_dm_hbb", "terrain_uber_4lyr_snowglt_pxm_hbb", "terrain_uber_4lyr_snowglt_spx_dm_hbb", "terrain_uber_4lyr_spx_dm_hbb_1111", 
-                "terrain_uber_4lyr_spx_dm_hbb_1121", "terrain_uber_4lyr_spx_dm_hbb_1221", "terrain_uber_4lyr_spx_dm_hbb_2111", "terrain_uber_4lyr_spx_dm_hbb_2221", 
-                "terrain_uber_4lyr_spx_dm_hbb", "terrain_uber_4lyr"]
+                "terrain_cb_w_4lyr_spec_int.sps", "terrain_cb_4lyr_lod.sps"]
     mask_only_terrains = ["terrain_cb_w_4lyr_cm.sps", "terrain_cb_w_4lyr_cm_tnt.sps",
                           "terrain_cb_w_4lyr_cm_pxm_tnt.sps", "terrain_cb_w_4lyr_cm_pxm.sps"]
     cutouts = ["cutout.sps", "cutout_um.sps", "cutout_tnt.sps", "cutout_fence.sps", "cutout_fence_normal.sps", "cutout_hard.sps", "cutout_spec_tnt.sps", "normal_cutout.sps",
                "normal_cutout_tnt.sps", "normal_cutout_um.sps", "normal_spec_cutout.sps", "normal_spec_cutout_tnt.sps", "trees_lod.sps", "trees.sps", "trees_tnt.sps",
-               "trees_normal.sps", "trees_normal_spec.sps", "trees_normal_spec_tnt.sps", "trees_normal_diffspec.sps", "trees_normal_diffspec_tnt.sps"]
+               "trees_normal.sps", "trees_normal_spec.sps", "trees_normal_spec_tnt.sps", "trees_normal_diffspec.sps", "trees_normal_diffspec_tnt.sps", "standard_cutout_hbb.sps"]
     alphas = ["normal_spec_alpha.sps", "normal_spec_reflect_alpha.sps", "normal_spec_reflect_emissivenight_alpha.sps", "normal_spec_screendooralpha.sps", "normal_alpha.sps",
               "normal_reflect_alpha.sps", "emissive_alpha.sps", "emissive_alpha_tnt.sps", "emissive_additive_alpha.sps", "emissivenight_alpha.sps", "emissivestrong_alpha.sps",
               "spec_alpha.sps", "spec_reflect_alpha.sps", "alpha.sps", "reflect_alpha.sps", "normal_screendooralpha.sps", "spec_screendooralpha.sps", "cloth_spec_alpha.sps",
-              "cloth_normal_spec_alpha.sps"]
+              "cloth_normal_spec_alpha.sps" , "standard_dirt_alpha.sps", "standard_dirt_ground.sps"]
     glasses = ["glass.sps", "glass_pv.sps", "glass_pv_env.sps", "glass_env.sps", "glass_spec.sps", "glass_reflect.sps", "glass_emissive.sps", "glass_emissivenight.sps",
                "glass_emissivenight_alpha.sps", "glass_breakable.sps", "glass_breakable_screendooralpha.sps", "glass_displacement.sps", "glass_normal_spec_reflect.sps",
-               "glass_emissive_alpha.sps"]
+               "glass_emissive_alpha.sps", "standard_glass_breakable.sps", "standard_glass_fp.sps", "standard_glass.sps"]
     decals = ["decal.sps", "decal_tnt.sps", "decal_glue.sps", "decal_spec_only.sps", "decal_normal_only.sps", "decal_emissive_only.sps", "decal_emissivenight_only.sps",
               "decal_amb_only.sps", "normal_decal.sps", "normal_decal_pxm.sps", "normal_decal_pxm_tnt.sps", "normal_decal_tnt.sps", "normal_spec_decal.sps", "normal_spec_decal_detail.sps",
               "normal_spec_decal_nopuddle.sps", "normal_spec_decal_tnt.sps", "normal_spec_decal_pxm.sps", "spec_decal.sps", "spec_reflect_decal.sps", "reflect_decal.sps", "decal_dirt.sps",
-              "mirror_decal.sps", "distance_map.sps"]
+              "mirror_decal.sps", "distance_map.sps" , "standard_decal_blend.sps" , "standard_decal.sps", "standard_decal_ground.sps", "standard_decal_hbb.sps","standard_decal_heightmap.sps", 
+              "standard_decal_normal_only.sps" , "standard_decal_tnt.sps"]
     veh_cutouts = ["vehicle_cutout.sps", "vehicle_badges.sps"]
     veh_glasses = ["vehicle_vehglass.sps", "vehicle_vehglass_inner.sps"]
     veh_decals = ["vehicle_decal.sps", "vehicle_decal2.sps",
@@ -122,6 +119,7 @@ class ShaderManager:
                   "emissivenight_geomnightonly.sps", "emissivestrong_alpha.sps", "emissivestrong.sps", "normal_spec_reflect_emissivenight_alpha.sps", "emissive_alpha.sps",
                   "emissive_alpha_tnt.sps", "emissive_additive_alpha.sps", "emissivenight_alpha.sps", "glass_emissive.sps", "glass_emissivenight.sps", "glass_emissivenight_alpha.sps",
                   "glass_emissive_alpha.sps", "decal_emissive_only.sps", "decal_emissivenight_only.sps"]
+
 
     def tinted_shaders():
         return ShaderManager.cutouts + ShaderManager.alphas + ShaderManager.glasses + ShaderManager.decals + ShaderManager.veh_cutouts + ShaderManager.veh_glasses + ShaderManager.veh_decals + ShaderManager.shadow_proxies
